c68df85479cd14ac16d7f6d482cecc336d99f371
Wataru Tsujimoto <tsujimoto.wataru@g.wakayama-u.jp>
2020-08-14 23:41:30 +0900

    get dropped tab

--- js/sidebarscript.js
+++ js/sidebarscript.js
@@ -1,14 +1,23 @@
 console.log("open panel");
-const dropEvent = function (event) {
+async function dropEvent(event) {
     console.log(event);
     console.log(event.dataTransfer.types[0]);
     console.log(event.dataTransfer.dropEffect);
+    if(event.dataTransfer.types[0]!="text/x-moz-text-internal") {
+        return;
+    }
+
+    const activeTabs = await browser.tabs.query({active:true});
+    console.log(activeTabs);
 };
+
+function handleDragOver(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+    evt.dataTransfer.dropEffect = 'copy';
+}
+
 const div = document.querySelector("div");
 console.log(div);
-div.addEventListener('dragstart', dropEvent);
-div.addEventListener('dragover', dropEvent);
-div.addEventListener('dragenter', dropEvent);
-div.addEventListener('dragleave', dropEvent);
-div.addEventListener('dragend', dropEvent);
-div.addEventListener('drop', dropEvent);
+div.addEventListener('dragover', handleDragOver);
+div.addEventListener('drop', dropEvent, false);
